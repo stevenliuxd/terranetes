51f4be9392dde2addd89d1d36644a982c1b86def
Steven Liu <shnauzerr@hotmail.com>
2023-11-10 14:50:08 -0700

    Add tf file

---
+++ main.tf
@@ -0,0 +1,53 @@
+provider "kubernetes" {
+  config_context_cluster   = minikube
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx-deployment"
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:latest"
+          name  = "nginx"
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-service"
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_deployment.nginx.spec[0].template[0].metadata[0].labels.app
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "NodePort"
+  }
+}
